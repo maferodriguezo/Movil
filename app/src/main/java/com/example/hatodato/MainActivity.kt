64238c97ccaec17bbd63d18ff1b5118980f20c33
Mafe Rodriguez <maferodriguez0617@gmail.com>
2025-09-01 19:19:40 -0500

    Initial commit: Proyecto Android HatoDato

---
+++ app/src/main/java/com/example/hatodato/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.hatodato
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Agregar el click listener al botón Ganadero
+        val btnGanadero = findViewById<View>(R.id.btnGanadero)
+        btnGanadero.setOnClickListener {
+            val intent = Intent(this, RegistrarGanaderoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hatodato/RegistrarGanaderoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hatodato
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrarGanaderoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_ganadero)
+    }
+}
