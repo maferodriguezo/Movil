3885384b4231906680d33e2613542657f0b91120
salmanzag <s.almanzag@javeriana.edu.co>
2025-09-01 20:43:54 -0500

    ya vuelvo a intentar

--- app/src/main/java/com/example/hatodato/RegistrarGanaderoActivity.kt
+++ app/src/main/java/com/example/hatodato/RegistrarGanaderoActivity.kt
@@ -1,12 +1,19 @@
 package com.example.hatodato
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 
+import androidx.appcompat.app.AppCompatActivity
+
+
 class RegistrarGanaderoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar_ganadero)
+
+
     }
+
+
 }
